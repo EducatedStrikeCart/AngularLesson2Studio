913349e4584a3cf15cc295dc43d46675a13bd2ae
EducatedStrikeCart <dhn004@gmail.com>
2021-09-09 19:14:25 -0400

    updated components

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -10,7 +10,9 @@ export class HeaderComponent implements OnInit {
   missionName: string = "Mars 2030";
   rocketName: string = "Plasma Max";
   editingMission: boolean = false;
-  
+  // add editing rocket
+  editingRocket: boolean = false;
+
   constructor() { }
 
   ngOnInit() {
@@ -20,5 +22,12 @@ export class HeaderComponent implements OnInit {
     this.missionName = updatedName;
     this.editingMission = false;
   }
+
+  //add updateRocket; copy of updateMission, changing missionName and editingMission to rocket variables
+  updateRocket(updatedName: string) {
+    this.rocketName = updatedName;
+    this.editingRocket = false;
+  }
+
 
 }
