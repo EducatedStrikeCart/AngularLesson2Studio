9925ad5e530459f029608941db55d64471af499c
EducatedStrikeCart <dhn004@gmail.com>
2021-09-09 20:45:08 -0400

    added button to add and remove equipment to equipment component

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -5,27 +5,56 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './equipment.component.html',
   styleUrls: ['./equipment.component.css']
 })
+
 export class EquipmentComponent implements OnInit {
-   equipmentItems: object[] = [
-       {name: 'Duct Tape', mass: 0.5},
-       {name: 'Space Camera', mass: 20},
-       {name: 'Food', mass: 150},
-       {name: 'Oxygen Tanks', mass: 400},
-       {name: 'AE-35 Unit', mass: 5},
-       {name: 'ISS Supplies', mass: 800},
-       {name: 'Water', mass: 250},
-       {name: 'Satellite', mass: 1200},
-       {name: 'R2 Unit', mass: 32}
-   ];
-   cargoHold: object[] = [];
-   cargoMass: number = 0;
-   maximumAllowedMass: number = 2000;
-   maxItems: number = 10;
+  equipmentItems: object[] = [
+    { name: 'Duct Tape', mass: 0.5 },
+    { name: 'Space Camera', mass: 20 },
+    { name: 'Food', mass: 150 },
+    { name: 'Oxygen Tanks', mass: 400 },
+    { name: 'AE-35 Unit', mass: 5 },
+    { name: 'ISS Supplies', mass: 800 },
+    { name: 'Water', mass: 250 },
+    { name: 'Satellite', mass: 1200 },
+    { name: 'R2 Unit', mass: 32 }
+  ];
+  cargoHold: object[] = [];
+  cargoMass: number = 0;
+  maximumAllowedMass: number = 2000;
+  maxItems: number = 10;
+  massBudget: number = this.maximumAllowedMass - this.cargoMass;
 
-   constructor() { }
+  constructor() { }
 
-   ngOnInit() { }
+  ngOnInit() { }
 
-   // Code your addItem function here:
-   
+
+
+  // Code your addItem function here:
+
+
+  addItem(input: equipment): boolean {
+    this.cargoHold.push(input);
+    this.cargoMass += input.mass;
+    return this.cargoMass < this.maximumAllowedMass + 200 || this.cargoMass < this.maximumAllowedMass - 200
+  }
+
+  isAddItemDisabled(input:equipment) {
+    return this.cargoHold.length === this.maxItems || input.mass + this.cargoMass > this.maximumAllowedMass
+  }
+
+  isNearMaxMass() {
+    return this.cargoMass > this.maximumAllowedMass - 300
+  }
+
+  setHoldEmpty() {
+    this.cargoHold = [];
+    this.cargoMass = 0;
+  }
+}
+
+
+interface equipment {
+  name: string,
+  mass: number
 }
